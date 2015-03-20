5e35870f3b4bf2d075811add82749ecf223dd177
embugge <erikmb@stud.ntnu.no>
2015-03-20 14:05:23 +0100

    Changed BG color on navbar

--- PushMe/app/controllers/Application.java
+++ PushMe/app/controllers/Application.java
@@ -21,21 +21,9 @@ public class Application extends Controller {
         return ok(login.render(Form.form(Login.class)));
     }
     
-    public static Result signup() {
-        return ok(signup.render(Form.form(User.class)));
-    }
+
     
-    public static Result createUser() {
-        Form<User> userForm = Form.form(User.class).bindFromRequest();
-        if(userForm.hasErrors()) {
-            return badRequest(
-                    views.html.signup.render(userForm)
-            );
-        } else {
-            User.create(userForm.get());
-            return redirect(routes.Application.login());
-        }
-    }
+   
     
     @Security.Authenticated(Secured.class)
     public static Result userInfo() {
--- PushMe/app/controllers/SignupController.java
+++ PushMe/app/controllers/SignupController.java
@@ -1,5 +1,32 @@
 package controllers;
 
-public class SignupController {
+import static play.data.Form.form;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import play.*;
+import play.data.*;
+import play.mvc.*;
+import models.*;
+import views.html.*;
+
+public class SignupController extends Controller {
+	
+    public static Result signup() {
+        return ok(signup.render(Form.form(User.class)));
+    }
+    
+    public static Result createUser() {
+        Form<User> userForm = Form.form(User.class).bindFromRequest();
+        if(userForm.hasErrors()) {
+            return badRequest(
+                    views.html.signup.render(userForm)
+            );
+        } else {
+            User.create(userForm.get());
+            return redirect(routes.Application.login());
+        }
+    }
+    
 }
