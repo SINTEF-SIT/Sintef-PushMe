594e0cd145318ca1c5b92c22646ecb70dcf3aff0
Emil <emilhe@stud.ntnu.no>
2015-04-10 15:26:39 +0200

    Profile route

--- PushMe/app/controllers/ProfileController.java
+++ PushMe/app/controllers/ProfileController.java
@@ -1,5 +1,22 @@
 package controllers;
 
+import user.java
+
+import static play.data.Form.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import play.*;
+import play.data.*;
+import play.mvc.*;
+import models.*;
+import views.html.*;
+
 public class ProfileController {
 
+    public static Result profile() {
+        return ok(userinfo.render(Form.form(User.class)));
+    }
+	
 }
