977904af5974c2974798a11c4a1399d605c98971
Emil <emilhe@stud.ntnu.no>
2015-04-10 15:26:09 +0200

    Temporary mail functionality
    "Untested"

--- PushMe/app/controllers/SignupController.java
+++ PushMe/app/controllers/SignupController.java
@@ -4,25 +4,20 @@ import static play.data.Form.form;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import play.*;
 import play.data.*;
 import play.mvc.*;
 import models.*;
 import views.html.*;
-
+import play.libs.Mail
 import static play.data.Form.form;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import play.*;
-import play.data.*;
-import play.mvc.*;
-import models.*;
-import views.html.*;
 
 public class SignupController extends Controller {
+	
+	
 	
     public static Result signup() {
         return ok(signup.render(Form.form(User.class)));
@@ -40,4 +35,22 @@ public class SignupController extends Controller {
         }
     }
     
+    public void sendValidationEmail () {
+    	SimpleEmail email = new SimpleEmail();
+    	email.setFrom("pushMe@sintef.no");
+    	email.addTo("test@test.test");
+    	email.setSubject("Confirmation code");
+    	
+    	char[5] code = createRandomCode();
+    	email.setMsg("Your confirmation PushMe confirmation code is: " + code + "\nYou can ignore this email if this don't consern you.");
+    	Mail.send(email);
+    }
+    
+    private char[] createRandomCode() {
+    	char[5] code;
+        Random r = new Random();
+        String alphabet = "0123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < 5; i++) {
+            code[i] = alphabet.charAt(r.nextInt(alphabet.length()));
+        } return code;
 }
