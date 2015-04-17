4de10e560f2b52fb19dd92d14d9b00c9868a4196
itishrem <m.itishree@gmail.com>
2015-04-17 16:12:39 +0200

    Test for profile

--- PushMe/app/controllers/ProfileController.java
+++ PushMe/app/controllers/ProfileController.java
@@ -12,10 +12,54 @@ import play.mvc.*;
 import models.*;
 import views.html.*;
 
-public class ProfileController {
+public class ProfileController extends Controller {
 
 //    public static Result profile() {
 //        return ok(userinfo.render(Form.form(User.class)));
 //    }
 	
+
+ @Security.Authenticated(Secured.class)
+ public static Result profile() {
+ 	User loggedOnUser = User.find.byId(request().username());
+ return ok(views.html.profileinfo.render(loggedOnUser, findUserInfo(loggedOnUser), Form.form(User.class), Form.form(Userinformation.class), ActivityLevel.all()));
+    }
+
+public static Userinformation findUserInfo(User user){
+	List<Userinformation> allUserinfo = Userinformation.all();
+	Userinformation userinfo = null;
+	for(int i=0;i<allUserinfo.size();i++){
+		if(allUserinfo.get(i).belongsTo == user){
+			userinfo = allUserinfo.get(i);
+		}
+	}
+	return userinfo;
 }
+ @Security.Authenticated(Secured.class)
+ public static Result updateUser()
+{
+	User current_user = User.find.byId(request().username());
+	Form<User> filledForm = Form.form(User.class).bindFromRequest();
+
+	filledForm.get().email = current_user.email;
+	filledForm.get().name = current_user.name;
+	User.create(filledForm.get());
+
+	return redirect(routes.DashboardController.dashboard());
+}
+
+@Security.Authenticated(Secured.class)
+public static Result updateUserInfo(){
+	Form<Userinformation> filledForm = Form.form(Userinformation.class).bindFromRequest();
+	User current_user = User.find.byId(request().username());
+	Form<Userinformation> oldForm = Form.form(Userinformation.class);
+	oldForm.fill(findUserInfo(current_user));
+
+	filledForm.get().belongsTo = current_user;
+	filledForm.get().dob = oldForm.get().dob;
+	filledForm.get().gender = oldForm.get().gender;
+	Userinformation.create(filledForm.get());
+	return redirect(routes.DashboardController.dashboard());
+}
+}
+
