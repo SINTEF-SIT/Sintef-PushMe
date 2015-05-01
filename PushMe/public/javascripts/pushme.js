14f889ff29d297474eac05aa0544d9c588fc1cb0
embugge <erikmb@stud.ntnu.no>
2015-05-01 14:30:08 +0200

    Moved some js

--- PushMe/public/javascripts/pushme.js
+++ PushMe/public/javascripts/pushme.js
@@ -5,3 +5,27 @@ function updateTextInput(val, low, med, high, name) {
 	else if(val==3){val = high}		      
 	document.getElementById(name).innerHTML = val;		    
 }
+
+/*Setting current date as default*/
+$(document).ready(function(year, month, day) {
+	 var d = new Date();
+
+	 var month = d.getMonth()+1;
+	 var day = d.getDate();
+
+	 var output = d.getFullYear() + '-' +
+	     (month<10 ? '0' : '') + month + '-' +
+	     (day<10 ? '0' : '') + day;
+	 
+		$('.date').val(output)
+	});		
+/*Use date checked in the calendar*/
+$(function () {
+   $("#datepicker").datepicker({
+       inline: true,
+       dateFormat: 'yy-mm-dd',
+       onSelect: function () {
+           $('.date').val(this.value)
+       }
+   });
+});
