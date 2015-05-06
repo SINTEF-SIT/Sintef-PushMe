90ed9ff77e6d7284e65671d6ca2d576a5cc2b921
embugge <erikmb@stud.ntnu.no>
2015-05-06 17:38:11 +0200

    Started on language changer

---
+++ PushMe/app/controllers/LanguageController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.*;
+
+public class LanguageController extends Controller {
+
+	public static Result changeLanguage(String lang){
+		Controller.changeLang(lang);
+		return IndexController.index();
+	}
+	
+}
