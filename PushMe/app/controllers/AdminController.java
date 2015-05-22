a49227d63237da88257c8abfd58619efd76f1c9f
embugge <erikmb@stud.ntnu.no>
2015-05-22 14:21:42 +0200

    Woop

--- PushMe/app/controllers/AdminController.java
+++ PushMe/app/controllers/AdminController.java
@@ -1,5 +1,18 @@
 package controllers;
 
-public class AdminController {
+import static play.data.Form.form;
 
+import play.*;
+import play.data.*;
+import play.mvc.*;
+import models.*;
+import views.html.*;
+
+public class AdminController extends Controller {
+
+	@Security.Authenticated(Secured.class)
+	public static Result calendar() {
+        return ok(adminStatistics.render(ProfileController.findUser()));
+    }
+	
 }
