bf3bfb06117f9442da2195606afb6d3bae9ae9f6
embugge <erikmb@stud.ntnu.no>
2015-05-30 22:40:50 +0200

    Test examples

--- PushMe/test/models/ModelsTest.java
+++ PushMe/test/models/ModelsTest.java
@@ -13,12 +13,21 @@ public class ModelsTest extends WithApplication {
         start(fakeApplication(inMemoryDatabase()));
     }
     
-    //Create and retrive a user
+    //Create and retrive an activity
     @Test
-    public void createAndRetrieveUser() {
-        new User("embugge@hotmail.com", "Bob", "secret").save();
-        User bob = User.find.where().eq("email", "embugge@hotmail.com").findUnique();
-        assertNotNull(bob);
-        assertEquals("Bob", bob.name);
-    }    
+    public void ActivityTest() {
+        new Activity("Running", 10, 20, 30).save();
+        Activity running = Activity.find.where().eq("name", "Running").findUnique();
+        assertNotNull(running);
+        assertEquals(10, running.low_intensity);
+    }   
+    
+  //Create and retrive an activity level
+    @Test
+    public void ActivityLevelTest() {
+        new ActivityLevel(1, 3, "High").save();
+        ActivityLevel al = ActivityLevel.find.where().eq("id", 1).findUnique();
+        assertNotNull(al);
+        assertEquals(3, al.level);
+    }
 }
